ed85daa053d0a464e13307b1a9c935576af6361e
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-05-30 05:53:12 +0300

    Get playlist url from input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from spotify import Spotify
 
 spotify = Spotify()
-playlist = spotify.get_playlist(playlist_url="")
+playlist = spotify.get_playlist(playlist_url=input("Enter playlist url: "))
 spotify.export_to_csv(playlist)
