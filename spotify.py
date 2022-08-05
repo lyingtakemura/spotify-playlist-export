21fc71dee502e96954a29fa59da06d9419ed9352
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-08-05 18:05:32 +0300

    Update __parse_playlist_id method

--- spotify.py
+++ spotify.py
@@ -14,31 +14,6 @@ class Spotify:
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
 
-    def get_access_token(self) -> str:
-        """
-        - encode client_id and client_secret to base64 string
-        - request spotify api access_token by resulted base64 string
-        """
-        b64string = "{}:{}".format(self.client_id, self.client_secret)
-        b64string = b64string.encode("ASCII")
-        b64string = b64encode(b64string)
-        b64string = bytes.decode(b64string)
-
-        headers = {
-            "Authorization": "Basic {}".format(b64string)
-        }
-        data = {
-            "grant_type": "client_credentials"
-        }
-
-        response = requests.post(
-            "https://accounts.spotify.com/api/token",
-            headers=headers,
-            data=data
-        )
-        result = response.json()
-
-        return result["access_token"]
 
     def get_playlist(self, playlist_url: str) -> list:
         """
@@ -49,12 +24,10 @@ class Spotify:
         - if 'next' is None: end of playlist reached, exit loop
         """
         result = []
-        playlist_id = playlist_url.split("/playlist/")[1]
-        playlist_id = playlist_id.split("?")[0]
-
-        url = "{}/playlists/{}/tracks".format(self.url, playlist_id)
+        
+        url = "{}/playlists/{}/tracks".format(self.url, self.__parse_playlist_id(playlist_url))
         headers = {
-            "Authorization": "Bearer {}".format(self.get_access_token()),
+            "Authorization": "Bearer {}".format(self.__get_access_token()),
             "grant_type": "access_token"
         }
 
@@ -84,3 +57,37 @@ class Spotify:
                 album = item["track"]["album"]["name"]
 
                 writer.writerow([name, artists, album])
+
+
+    def __get_access_token(self) -> str:
+        """
+        - encode client_id and client_secret to base64 string
+        - request spotify api access_token by resulted base64 string
+        """
+        b64string = "{}:{}".format(self.client_id, self.client_secret)
+        b64string = b64string.encode("ASCII")
+        b64string = b64encode(b64string)
+        b64string = bytes.decode(b64string)
+
+        headers = {
+            "Authorization": "Basic {}".format(b64string)
+        }
+        data = {
+            "grant_type": "client_credentials"
+        }
+
+        response = requests.post(
+            "https://accounts.spotify.com/api/token",
+            headers=headers,
+            data=data
+        )
+        result = response.json()
+
+        return result["access_token"]
+
+    def __parse_playlist_id(self, playlist_url: str) -> str:
+        playlist_id = playlist_url.split("/playlist/")[1]
+        print(playlist_id)
+        playlist_id = playlist_id.split("?")[0]
+        print(playlist_id)
+        return playlist_id
