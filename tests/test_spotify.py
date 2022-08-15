4df4f1f83a0ee30afe082f7ba047e695f5a3a49f
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-08-15 17:53:46 +0300

    Update to_b64_string test

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -1,6 +1,19 @@
-def func(x):
-    return x + 1
+import pytest
+from spotify import Spotify
 
 
-def test_answer():
-    assert func(4) == 5
+@pytest.fixture
+def spotify_object():
+    spotify = Spotify()
+    return spotify
+
+
+def test_spotify_object_setup(spotify_object):
+    assert spotify_object.url is not None
+    assert spotify_object.client_id is not None
+    assert spotify_object.client_secret is not None
+
+
+def test_to_b64_string(spotify_object):
+    assert spotify_object._to_b64_string("qq", "ww") == "OWIxZjI0ZGNlNjU1NGZlOTgyNWM1ZjNlMmFlYjhmZmU6ZDMyYTA0Y2FiNjdjNGRiOTk0YmI2ODY1NDYyZTcxOTk="
+
