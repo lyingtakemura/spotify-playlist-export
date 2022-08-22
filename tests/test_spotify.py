b1146ae3b3684743197150fe6fa90be5da33ecb8
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-08-22 17:19:13 +0300

    Update parse_playlist_id test

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -6,23 +6,21 @@ from spotify import Spotify
 
 @pytest.fixture
 def spotify():
-    return Spotify()
+    spotify = Spotify()
+    return spotify
 
 
 urls = [
-        ("https://open.spotify.com/playlist/60S2Vw5q3DPjrYMbLezJa3?si=1658078b7f1846d4", "60S2Vw5q3DPjrYMbLezJa3"),
-        ("https://open.spotify.com/playlist/60S2Vw5q3DPjrYMbLezJa3", "60S2Vw5q3DPjrYMbLezJa3"),
-        ("https://open.spotify.com/playlist/00000000000000000", "00000000000000000"),
-        ("https://open.spotify.com/playlist/000000000?si=0000000000", "000000000")
+        ("https://open.spotify.com/playlist/qqqqqqqq?si=qqqqqqqq", "qqqqqqqq"),
+        ("https://open.spotify.com/playlist/qqqqqqqqqq", "qqqqqqqqqq"),
+        ("https://open.spotify.com/playlist/000000000000", "000000000000"),
+        ("https://open.spotify.com/playlist/0000000?si=0000000", "0000000")
     ]
 
 
 @pytest.mark.parametrize("url, expected", urls)
-def test_parse_playlist_id(spotify, monkeypatch, url, expected):
+def test_parse_playlist_id(spotify, mocker, url, expected):
     playlist_url = StringIO(url)
-    monkeypatch.setattr('sys.stdin', playlist_url)
-    assert spotify._parse_playlist_id() == expected
-
-
-def test_to_b64_string(spotify):
-    assert spotify._to_b64_string("qq", "ww") == "OWIxZjI0ZGNlNjU1NGZlOTgyNWM1ZjNlMmFlYjhmZmU6ZDMyYTA0Y2FiNjdjNGRiOTk0YmI2ODY1NDYyZTcxOTk="
+    mocker.patch('sys.stdin', playlist_url)
+    spotify._parse_playlist_id()
+    assert spotify.playlist_id == expected
