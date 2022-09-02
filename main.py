532f41d626403388f9ab5c1caa81044bdf117497
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-02 22:10:35 +0300

    Add logging

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import logging
+
 from spotify import ExportToCSV, ExportToJSON, Spotify
 
-spotify = Spotify()
-spotify.get_playlist()
-spotify.export_playlist(ExportToCSV())
-spotify.export_playlist(ExportToJSON())
+logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(funcName)s :: %(lineno)d \
+:: %(message)s', level=logging.DEBUG)
+
+
+def main():
+    spotify = Spotify()
+    spotify.get_playlist()
+    spotify.export_playlist(ExportToCSV())
+    spotify.export_playlist(ExportToJSON())
+
+
+if __name__ == "__main__":
+    logging.debug(main)
+    main()
