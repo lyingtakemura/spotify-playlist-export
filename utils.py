7fb417aa69899de83bd61d32c8848b79b684ff02
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-11-29 23:06:22 +0200

    Replace venv with poetry

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import datetime
 
 
 def timeit(func):
-
     def wrapper(*args, **kwargs):
         start = datetime.datetime.now()
         result = func(*args, **kwargs)
@@ -10,4 +9,5 @@ def timeit(func):
         time = end - start
         print("{} RUN TOOK: {}".format(func.__name__.upper(), time))
         return result
+
     return wrapper
